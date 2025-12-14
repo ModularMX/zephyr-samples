5ff1fae24edb742a62ec149566e27b826658dd30
DiegoMX <diego.perez@modular-mx.com>
2025-12-14 21:02:32 +0000

    template project

---
+++ template/src/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file main.c
+ * @brief Template sample application main file
+ * 
+ * Write in here a brief description of the sample code functionality.
+ */
+#include <zephyr/kernel.h>
+
+int main( void )
+{
+    
+    while(1)
+    {
+    }
+    
+    return 0;
+}
