9e034d98845f997e6845945c33d39e69c79f4706
DiegoMX <diego.perez@modular-mx.com>
2025-12-14 23:19:34 +0000

    pin toggle sample

---
+++ hal_drivers/GPIOS/_01_toggle_pin/src/main.c
@@ -0,0 +1,45 @@
+/**
+ * @file main.c
+ * @brief Toggle one pin using port function
+ * 
+ * This sample application shows how to toggle a pin using the port function
+ * gpio_pin_toggle(). The pin to toggle is defined based on the board in use.
+ * - For the nrf54l15dk board, pin 9 from port 2 is toggled every 300ms.
+ * - From the nucleo_g0b1re board, pin 5 from port A is toggled every 300ms.
+ * - From the native_sim board, pin 0 from port 0 is toggled every 300ms.
+ */
+/* Include libraries */
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+
+/*we are using pin functions with no access to device tree, so the only
+way to achieve hardware agnostic is use adefine with the pin to use 
+according to board in use*/
+#ifdef CONFIG_SOC_NRF54L15
+    #define PORT_PIN 9
+#elif CONFIG_SOC_STM32G0B1XX
+    #define PORT_PIN 5
+#elif CONFIG_SOC_POSIX
+    #define PORT_PIN 0
+#else
+    #error "Define the pin to toggle for your board"
+#endif 
+
+/* Get the device descriptor for port with the pin connected */
+const struct device *port = DEVICE_DT_GET( DT_ALIAS(gpio) );
+
+int main( void )
+{
+    /* Configure pin as output */
+    gpio_pin_configure( port, PORT_PIN, GPIO_OUTPUT);
+    
+    while(1)
+    {
+        /* Toggle pin from port using a port function */
+        gpio_pin_toggle( port, PORT_PIN );
+        /* 300ms Delay */
+        k_msleep( 300 );
+    }
+    return 0;
+}
